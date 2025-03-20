a6513e0d688c6bd687b5909135ede2e72472bd48
Zane Mitrevica <zane.mitrevica@gmail.com>
2025-03-20 11:02:15 +0200

    Switch to io StringIO

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import scipy
 import re
+from io import StringIO
 import requests
 import dash
 from dash import dcc, html, Input, Output, State
@@ -12,14 +13,14 @@ np.random.seed(42)
 PUB_DATA_URL = "https://raw.githubusercontent.com/zanemit/QS-papers-citations-interactive/main/data/publication_data.csv"
 response_pub = requests.get(PUB_DATA_URL)
 if response_pub.status_code==200:
-    pub_df = pd.read_csv(pd.compat.StringIO(reponse_pub.text), index_col=0)
+    pub_df = pd.read_csv(StringIO(reponse_pub.text), index_col=0)
 else:
     print("Failed to download publication data!")
 
 ASJC_DATA_URL = "https://raw.githubusercontent.com/zanemit/QS-papers-citations-interactive/main/data/asjc_data.csv"
 response_asjc = requests.get(ASJC_DATA_URL)
 if response_pub.status_code==200:
-    asjc_df = pd.read_csv(pd.compat.StringIO(reponse_asjc.text), index_col=0)
+    asjc_df = pd.read_csv(StringIO(reponse_asjc.text), index_col=0)
 else:
     print("Failed to download asjc data!")
 
